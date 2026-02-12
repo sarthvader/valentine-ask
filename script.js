ff89370067b3d11a4464517eac4937f50fb24167
Sarthak Bhatnagar <sarthakb3000@gmail.com>
2026-02-11 18:07:33 -0800

    update with dog and frog

--- script.js
+++ script.js
@@ -62,7 +62,7 @@ noBtn.addEventListener("mouseover", () => {
 yesBtn.addEventListener("click", () => {
     title.textContent = "Yippeeee!";
 
-    catImg.src = "cat_dance.gif";
+    catImg.src = "frog_hearts.gif";
 
     document.querySelector(".letter-window").classList.add("final");
 
